c6469696782d6ed435e61cdb85378ad27cc2afb2
Nikita-Swami <nikitaswami09@gmail.com>
2025-03-23 15:24:49 +0530

    tttt

---
+++ src/Selenium_Program/test_Lab020.py
@@ -0,0 +1,33 @@
+import allure
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import time
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.common.action_chains import ActionChains, ActionBuilder
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.actions.mouse_button import MouseButton
+
+
+@allure.title("JS")
+@allure.description("Verify JS")
+def test_verify_vwo_js():
+    # ChromeOptions - --incognito
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument("--incognito")
+
+
+    driver = webdriver.Chrome(chrome_options)
+    driver.get("https://app.vwo.com/")
+    driver.maximize_window()
+
+    #Syncronously executes JavaScript in the current Window/frame
+
+    driver.execute_script("alert(1)")
+
+
+
+    time.sleep(5)
+    driver.quit()
